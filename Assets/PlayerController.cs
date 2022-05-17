c6499950727cd18bc188340a25fbf966a050e1df
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-05-17 14:16:55 -0300

    PlayerController

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < 17)
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+    }
+}
