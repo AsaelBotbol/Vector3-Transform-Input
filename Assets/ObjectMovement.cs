c7d98294e053075e3634f62f89450d9e9a2acd4f
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-05-17 13:49:54 -0300

    eulerAngles

---
+++ Assets/ObjectMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+
+    public Vector3 movement;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.transform.eulerAngles = movement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
