1e5cb5d06185b874a839d64f03de96f3d4316e79
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-05-17 13:52:48 -0300

    3 vectores

--- Assets/ObjectMovement.cs
+++ Assets/ObjectMovement.cs
@@ -6,12 +6,16 @@ public class ObjectMovement : MonoBehaviour
 {
 
     public Vector3 movement;
+    public Vector3 rotationAmount;
+    public Vector3 scaleAmount;
     public GameObject cube;
 
     // Start is called before the first frame update
     void Start()
     {
-        cube.transform.localScale = movement;
+        cube.transform.position = movement;
+        cube.transform.eulerAngles = rotationAmount;
+        cube.transform.localScale = scaleAmount;
     }
 
     // Update is called once per frame
